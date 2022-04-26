4f43693974db17f0e69eb9beceab7a3477e5061d
yousseffabid <abidyoussef58@gmail.com>
2022-04-26 12:26:13 +0100

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    string_number = str(number)
+    last_character = string_number[-1]
+    print('{:d}'.format(int(last_character)), end='')
+
+    return int(last_character)
