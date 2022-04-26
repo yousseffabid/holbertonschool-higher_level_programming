d96ceb67059923e71efb717d1c3d4d25bded42d7
yousseffabid <abidyoussef58@gmail.com>
2022-04-26 12:55:07 +0100

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 
 
 def fizzbuzz():
-    for number in range(1, 100):
+    for number in range(1, 101):
         if number % 15 == 0:
             print('FizzBuzz ', end='')
         elif number % 5 == 0:
