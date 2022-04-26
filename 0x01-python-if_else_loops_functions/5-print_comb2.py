7faabdbe9011e685e9e6a365458a9435a7525e9b
yousseffabid <abidyoussef58@gmail.com>
2022-04-26 20:56:19 +0100

    print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for number in range(0, 100):
     if number == 99:
-        print('{}'.format(number))
+        print(f'{number}')
     else:
-        print('{:02}, '.format(number), end='')
+        print(f'{number:02}, ', end='')
