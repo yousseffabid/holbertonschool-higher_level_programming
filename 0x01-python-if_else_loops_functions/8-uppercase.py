bad1fef1d701eefb53ffd1950810d45fba46d184
yousseffabid <abidyoussef58@gmail.com>
2022-04-26 12:12:06 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for character in str:
+        if ord(character) <= ord('z') and ord(character) >= ord('a'):
+            tmp = chr(ord(character) - 32)
+        else:
+            tmp = character
+        print('{}'.format(tmp), end='')
+
+    print()
