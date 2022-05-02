059b614c5f0999572e13e8b7fe82a3a26d6d4128
yousseffabid <abidyoussef58@gmail.com>
2022-05-02 13:49:41 +0100

    data sturcture

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            for index, element in enumerate(row):
+                if index != len(row) - 1:
+                    print(f'{element:d }', end='')
+                else:
+                    print(f'{element:d}')
+
+
+
+
