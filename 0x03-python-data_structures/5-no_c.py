7de89996dda473d26d71c24840a49780929b0aac
yousseffabid <abidyoussef58@gmail.com>
2022-05-02 13:37:03 +0100

    data structure

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    no_c = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            no_c += char
+    if no_c:
+        return no_c
+    else:
+        return my_string
