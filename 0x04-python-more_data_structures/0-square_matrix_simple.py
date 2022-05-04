591c53ffcd08757267614c94016d92976920ca09
yousseffabid <abidyoussef58@gmail.com>
2022-05-04 11:12:36 +0100

    data structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new_matrix = []
+        for row in matrix:
+            new_row = list(map(lambda x: x**2, row))
+            new_matrix.append(new_row)
+        return new_matrix
+    else:
+        return None
