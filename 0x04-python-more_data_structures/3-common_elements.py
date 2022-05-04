82a560414f9a608bfc261271d2e9693894f08712
yousseffabid <abidyoussef58@gmail.com>
2022-05-04 12:15:53 +0100

    data structures

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 or set_2:
-        return set.intersection(set_1, set_2)
-    else:
-        return None
+    return set.intersection(set_1, set_2)
