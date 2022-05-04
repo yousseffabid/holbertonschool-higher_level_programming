dae07b7a73de6eabb2220f0ec96ce55981604b4f
yousseffabid <abidyoussef58@gmail.com>
2022-05-04 21:37:20 +0100

    data structure

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,8 @@ def roman_to_int(roman_string):
     else:
         number = 0
         flag = 0
-        my_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        my_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
+                   "C": 100, "D": 500, "M": 1000}
         for i in range(len(roman_string) - 1, -1, -1):
             if roman_string[i] != "I":
                 flag = 1
