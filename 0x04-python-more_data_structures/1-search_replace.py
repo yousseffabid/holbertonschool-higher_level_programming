fbac2dae5fed719464c3ad3b94db71f1d5cc6558
yousseffabid <abidyoussef58@gmail.com>
2022-05-04 12:29:05 +0100

    data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,5 @@
 def search_replace(my_list, search, replace):
     if my_list:
         new_list = [x if search != x else replace for x in my_list]
-    return new_list
+        return new_list
+    return None
