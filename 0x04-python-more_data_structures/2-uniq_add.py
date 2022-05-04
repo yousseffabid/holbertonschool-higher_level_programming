72363c211d2e298533dfaefc5ab2628234b00068
yousseffabid <abidyoussef58@gmail.com>
2022-05-04 12:08:48 +0100

    data structures

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if my_list:
+        my_set = set(my_list)
+        result = 0
+        for x in my_set:
+            result += x
+        return result
+    return None
