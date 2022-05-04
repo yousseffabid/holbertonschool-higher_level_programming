870e01741c89eb9ac81c420bc94df5ad98a9916f
yousseffabid <abidyoussef58@gmail.com>
2022-05-04 13:25:16 +0100

    data structure

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if a_dictionary:
+        new_dict = {key: element*2 for key, element in a_dictionary}
+        return new_dict
+    return a_dictionary
