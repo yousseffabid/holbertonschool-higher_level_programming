58f0b0d7c36f3a8c67f0de7278980e78a210a62c
yousseffabid <abidyoussef58@gmail.com>
2022-05-04 20:44:22 +0100

    data structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        key_list = list(a_dictionary.keys())
-        val_list = list(a_dictionary.values())
-        maximum = max(val_list)
-        return key_list[val_list.index(maximum)]
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
     else:
         return None
