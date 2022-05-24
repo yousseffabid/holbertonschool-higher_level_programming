59b8c3d193aeefcc2752a5a638e362366e85aa5f
yousseffabid <abidyoussef58@gmail.com>
2022-05-24 23:02:02 +0100

    more classes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""LockedClass
+"""
+
+
+class LockedClass(object):
+    """LockedClass Class
+    """
+    __slots__ = 'first_name'
+
+    def __init__(self, first_name="") -> None:
+        self.first_name = first_name
