be18128c7ea84b6b8714c92e2f48ed7434fef54d
yousseffabid <abidyoussef58@gmail.com>
2022-05-30 16:07:21 +0100

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,6 +11,6 @@ class Square(Rectangle):
         Args:
             size (int): The size of the new square.
         """
-        self.integer_validator("size", size)
+        super().integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size  
+        self.__size = size
