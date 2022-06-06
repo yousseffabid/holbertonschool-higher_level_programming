7f07e31fc99a59037207160a4b64746db597b50f
yousseffabid <abidyoussef58@gmail.com>
2022-06-06 12:33:24 +0100

    almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle Class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize object's attribute
         args:
