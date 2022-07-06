e870e5246328f3d316e8b4b6469a18164230a974
yousseffabid <abidyoussef58@gmail.com>
2022-07-06 23:08:22 +0100

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,5 @@
 -- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
 -- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT tv_shows.`title`, tv_show_genres.`genre_id`
-  FROM `tv_shows` AS s
-        INNER JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
