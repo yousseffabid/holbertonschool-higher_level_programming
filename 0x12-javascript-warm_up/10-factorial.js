c135c089f692cccac4a017fe824e70ae1b3cb3fd
yousseffabid <abidyoussef58@gmail.com>
2022-08-01 16:07:24 +0100

    js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-const process = require('process');
-const args = process.argv;
-
-if (args.length === 2) {
-  console.log(1);
-} else {
-  const number = parseInt(args[2]);
-  let output = 1;
-  for (let i = 2; i <= number; i++) {
-    output *= i;
+function factorial (num) {
+  if (!num) {
+    return (1);
   }
-  console.log(output);
+  return (num * factorial(num - 1));
 }
+console.log(factorial(parseInt(process.argv[2])));
