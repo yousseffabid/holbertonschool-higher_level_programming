0d3b6732b5df73b3189928489c0bf6bb5d1622be
yousseffabid <abidyoussef58@gmail.com>
2022-08-01 15:55:48 +0100

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 const process = require('process');
 const args = process.argv;
-let arg1, arg2;
-arg1 = args[2];
-arg2 = args[3];
-console.log(arg1 + ' is ' + arg2);
+console.log(args[2] + ' is ' + args[3]);
