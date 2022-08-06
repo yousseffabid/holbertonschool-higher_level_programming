09a7c94e9fd946f273671cc7dc21151acdcb208a
yousseffabid <abidyoussef58@gmail.com>
2022-08-06 10:45:21 +0100

    orm

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""prints all City objects from the database"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""class city"""
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
