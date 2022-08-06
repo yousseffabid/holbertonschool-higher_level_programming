c3ddbcbeb36605a8c12c8d4eda428a6a2aa3bbc8
yousseffabid <abidyoussef58@gmail.com>
2022-08-06 10:30:37 +0100

    orm

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,12 +7,12 @@ from model_state import State
 
 
 if __name__ == "__main__":
-    path_name = "mysql+mysqldb://{}:{}@localhost/{}:3306". \
+    path_name = "mysql+mysqldb://{}:{}@localhost/{}". \
             format(sys.argv[1], sys.argv[2], sys.argv[3])
 
     engine = create_engine(path_name, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for s in session.query(State).order_by(State.id):
+    for s in session.query(State).all():
         print("{}: {}".format(s.id, s.name))
