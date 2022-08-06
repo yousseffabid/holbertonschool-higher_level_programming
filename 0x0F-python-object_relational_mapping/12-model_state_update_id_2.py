e8437d2c8907627cf816504d06309b8a4694b44e
yousseffabid <abidyoussef58@gmail.com>
2022-08-06 10:39:21 +0100

    orm

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""changes the name of a State object from the database"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 
 if __name__ == "__main__":
