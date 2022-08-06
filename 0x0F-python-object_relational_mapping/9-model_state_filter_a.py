3a1aa5b7035c13018ca8e820bb2ba3a36709401c
yousseffabid <abidyoussef58@gmail.com>
2022-08-06 10:34:01 +0100

    orm

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""lists all State objects that contain the letter a"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 
 if __name__ == "__main__":
