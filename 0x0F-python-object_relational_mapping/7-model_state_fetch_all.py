cf12792efcf437d4ac5bed9c03997a506219bbc8
yousseffabid <abidyoussef58@gmail.com>
2022-08-06 09:34:37 +0100

    rom

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,7 +6,7 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    path_name = "mysql+mysqldb://{}:{}@localhost/{}". \
+    path_name = "mysql+mysqldb://{}:{}@localhost/{}:3306". \
             format(sys.argv[1], sys.argv[2], sys.argv[3])
 
     engine = create_engine(path_name)
