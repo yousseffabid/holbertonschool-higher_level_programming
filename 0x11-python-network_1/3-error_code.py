c64162d85d23346338125f2152365abceda63216
yousseffabid <abidyoussef58@gmail.com>
2022-08-19 15:18:12 +0100

    fixed

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,12 +5,13 @@ displays the body of the response (decoded in utf-8)
 """
 from urllib.request import urlopen
 import urllib.error
+import urllib.request
 import sys
 
 
 if __name__ == "__main__":
     try:
         with urlopen(sys.argv[1]) as response:
-            print(response.read().encode("utf-8"))
+            print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as error:
         print("Error code: {}".format(error.code))
