05b4cca73559f9c7672c6954c1f7b6956e1e4512
yousseffabid <abidyoussef58@gmail.com>
2022-09-13 19:24:20 +0100

    fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  console.error(err || data);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
