17d70de8c6170afa16ef389a08b13ad52485b519
yousseffabid <abidyoussef58@gmail.com>
2022-09-13 19:40:40 +0100

    fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const axios = require('axios').default;
 const reqUrl = process.argv[2];
-let counter = 0;
+const dict = {};
+let i = 0;
 axios.get(reqUrl)
   .then(function (response) {
-    for (let i = 0; i < response.data.count; i++) {
-      if (String(response.data.results[i].characters).includes('18')) {
-        counter = counter + 1;
+    for (i = 0; i < response.data.length; i++) {
+      if (!dict[response.data[i].userId]) {
+        dict[response.data[i].userId] = 0;
+      }
+      if (response.data[i].completed === true) {
+        dict[response.data[i].userId] += 1;
       }
     }
-    console.log(counter);
-  })
-  .catch(function (error) {
-    console.error(error);
+    console.log(map);
   });
