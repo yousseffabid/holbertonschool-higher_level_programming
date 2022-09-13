3abc1bedab4ab0c2f718d582af195ed8a05852e6
yousseffabid <abidyoussef58@gmail.com>
2022-09-13 19:27:15 +0100

    fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const f = require('f');
-f.writeFile(process.argv[2], process.argv[3], (err, data) => {
-  if (err) {
-    console.error(err);
-  }
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
